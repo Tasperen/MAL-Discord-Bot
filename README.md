bdb9b002c9a1180eeaef1cb20744c626dd15f6cf
Alex Taber <alex@ataber.pw>
2016-10-04 12:30:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# MAL Discord Bot
+
+## Installation
+
+Install required modules:
+
+ * dicsordie
+ * xml2js
+ * request
+
+Change settings in config.json to add your MAL username, MAL password, and discord bot_token
+
+## Usage
+
+`!anime <Search string>`
+
+This will return a list of animes, sorted by number. Simply send the number of the details you want for the next command
---
+++ bot.js
@@ -0,0 +1,67 @@
+const Discordie = require('discordie');
+const client = new Discordie();
+
+const config = require('./config');
+const https = require('http');
+const parseXML = require('xml2js').parseString;
+const url = require('url');
+const request = require('request');
+
+var lastSearch;
+
+function searchAPI(searchString, channel, callback) {
+    var result = "";
+
+    const requestData = {
+        url: 'http://www.myanimelist.net/api/anime/search.xml?q=' + searchString,
+        headers: {
+            Authorization: 'Basic ' + new Buffer(config.mal_username + ':' + config.mal_password).toString('base64')
+        }
+    }
+
+    request(requestData, function(err, response, body) {
+
+        if (err) throw err;
+
+        var data;
+
+        parseXML(body, function(err, result) {
+            if (err) throw err;
+
+            data = result;
+        });
+
+        lastSearch = data.anime;
+
+        for (var i = 0; i < data.anime.entry.length; i++) {
+            result += (i + 1) + ": " + data.anime.entry[i].title + "\n";
+        }
+
+        callback(result, channel);
+    });
+}
+
+function sendMessage(content, channel) {
+    channel.sendMessage(content);
+}
+
+client.connect({ token: config.bot_token });
+
+client.Dispatcher.on("MESSAGE_CREATE", function(event) {
+    var message = event.message;
+
+    if (message.content.substring(0,6) === "!anime") {
+        searchAPI(message.content.substring(7), message.channel, sendMessage);
+    }
+
+    if (!isNaN(message.content)) {
+        if (lastSearch) {
+            if (+message.content - 1 >= 0 && +message.content - 1 < lastSearch.entry.length) {
+                var animeObject = lastSearch.entry[+message.content - 1];
+                var messageString = "**Title: " + animeObject.title + "**\n\n**Description**: " + animeObject.synopsis.toString().replace(/<br \/>/g, "") + "\n\n**URL**: http://myanimelist.net/anime/" + animeObject.id;
+                sendMessage(messageString, message.channel);
+                lastSearch = null;
+            }
+        }
+    }
+});
